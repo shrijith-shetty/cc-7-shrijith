ea7ead0788d08fde3d7fdf68bba47e99eb5a154f
shrijith_shetty <shrijith2910@gmail.com>
2026-03-13 08:09:21 +0530

    complete first 6 task and tested using Vitest

---
+++ assignment03/promise03.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+import { getSize } from "./promise03";
+
+describe("getFileSize", () => {
+  it("should throw error if file does not exist", async () => {
+    await expect(getSize("wrong-file.ts")).rejects.toThrow();
+  });
+});
+
+describe("getFileSize", () => {
+  it("should return file size", async () => {
+    const size = await getSize("assignment03/promise03.ts");
+
+    expect(size).toBeGreaterThan(0);
+  });
+});
